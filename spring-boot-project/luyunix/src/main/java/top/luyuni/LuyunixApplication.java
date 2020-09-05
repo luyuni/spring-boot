78e265a503aa651ba7141f0ae189a845c49cb2fd
niyulu <niyulu@weidian.com>
2020-09-05 14:12:09 +0800

    add luyunix

---
+++ spring-boot-project/luyunix/src/main/java/top/luyuni/LuyunixApplication.java
@@ -0,0 +1,11 @@
+package top.luyuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuyunixApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LuyunixApplication.class, args);
+	}
+}
